d456c0ac3f0e3e0fa8671c27cd9b2addc2fee082
Pablo-Oliveira18 <pablogui1611@gmail.com>
2021-01-10 16:35:52 -0300

    Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,8 @@ Future<void> main() async {
   runApp(MyApp());
 
   await Firebase.initializeApp();
+
+  FirebaseFirestore instaces = FirebaseFirestore.instance;
 }
 
 class MyApp extends StatelessWidget {
