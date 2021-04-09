d479eac63a0be12eee985643d26dc5c22d558d8a
Pablo Oliveira <pablogui1611@gmail.com>
2021-04-09 08:01:46 -0300

    Pop Up Home

--- lib/screen/home/components/item_tile.dart
+++ lib/screen/home/components/item_tile.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:controlsport_app_ecommerce/models/home/home_manager.dart';
 import 'package:controlsport_app_ecommerce/models/products/product_manager.dart';
+import 'package:controlsport_app_ecommerce/models/section/section.dart';
 import 'package:controlsport_app_ecommerce/models/section/section_item.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +16,7 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final homeManager = context.watch<HomeManager>();
     return GestureDetector(
       onTap: () {
         if (item.product != null) {
@@ -24,6 +27,27 @@ class ItemTile extends StatelessWidget {
           }
         }
       },
+      onLongPress: homeManager.editing
+          ? () {
+              showDialog(
+                  context: context,
+                  builder: (_) {
+                    return AlertDialog(
+                      title: const Text('Editar Item'),
+                      actions: <Widget>[
+                        FlatButton(
+                          onPressed: () {
+                            context.read<Section>().removeItem(item);
+                            Navigator.of(context).pop();
+                          },
+                          textColor: Colors.red,
+                          child: const Text('Excluir'),
+                        ),
+                      ],
+                    );
+                  });
+            }
+          : null,
       child: AspectRatio(
         aspectRatio: 1,
         child: item.image is String
