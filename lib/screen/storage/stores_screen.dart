e86955606113f5e81c9be90dbf01c4c60223cdd2
Pablo Guilherme Oliveira <pablogui1611@gmail.com>
2021-05-05 23:47:26 -0300

    140. Exibindo as Lojas

--- lib/screen/storage/stores_screen.dart
+++ lib/screen/storage/stores_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
+import 'components/store_card.dart';
+
 class StoresScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,7 +17,19 @@ class StoresScreen extends StatelessWidget {
       ),
       body: Consumer<StoresManager>(
         builder: (_, storesManager, __) {
-          return Container();
+          if (storesManager.stores.isEmpty) {
+            return LinearProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Colors.white),
+              backgroundColor: Colors.transparent,
+            );
+          }
+
+          return ListView.builder(
+            itemCount: storesManager.stores.length,
+            itemBuilder: (_, index) {
+              return StoreCard(storesManager.stores[index]);
+            },
+          );
         },
       ),
     );
