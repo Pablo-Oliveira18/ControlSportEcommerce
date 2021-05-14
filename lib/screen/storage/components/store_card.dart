fcfabb77b7348ac17bcccaeba6416ee4f2a0e7b5
Pablo Guilherme Oliveira <pablogui1611@gmail.com>
2021-05-13 21:34:41 -0300

    mudanças exibi lojas

--- lib/screen/storage/components/store_card.dart
+++ lib/screen/storage/components/store_card.dart
@@ -11,62 +11,99 @@ class StoreCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
 
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-      child: Column(
-        children: <Widget>[
-          Image.network(store.image),
-          Container(
-            height: 140,
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        store.name,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          fontSize: 17,
-                        ),
-                      ),
-                      Text(
-                        store.addressText,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                      ),
-                      Text(
-                        store.openingText,
-                        style: const TextStyle(
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8, 10, 8, 10),
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+            color: Colors.yellow[100],
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 16, 8, 8),
+              child: Column(
+                children: [
+                  Center(
+                    child: Text(
+                      "NOSSA MISSÃO, VISÃO E VALORES",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
                   ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    CustomIconButton(
-                      iconData: Icons.map,
-                      color: primaryColor,
-                      onTap: () {},
-                    ),
-                    CustomIconButton(
-                      iconData: Icons.phone,
-                      color: primaryColor,
-                      onTap: () {},
-                    ),
-                  ],
-                )
-              ],
+                  Divider(
+                    height: 15,
+                  ),
+                  Text(
+                      "Ser a melhor loja de moda masculina, com os melhores produtos, serviços e atendimento a seus clientes."),
+                  SizedBox(height: 10),
+                  Text(
+                      "Buscar a satisfação total de nossos clientes, garantindo atendimento com excelência profissional e técnica, fornecendo os melhores produtos, aplicar preços justos."),
+                  SizedBox(height: 10),
+                  Text(
+                      "Garantir que o atendimento aos nossos clientes, mesmo no site para compras eletrônicas, seja do mais alto padrão, oferecendo nosso contato por telefone, skype, chat ou e-mail, prestando todo e qualquer esclarecimento solicitado pelo cliente."),
+                ],
+              ),
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+        Card(
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          child: Column(
+            children: <Widget>[
+              Divider(),
+              Image.network(store.image),
+              Container(
+                height: 140,
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            store.name,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              fontSize: 17,
+                            ),
+                          ),
+                          Text(
+                            store.addressText,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                          Text(
+                            store.openingText,
+                            style: const TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        CustomIconButton(
+                          iconData: Icons.location_on_outlined,
+                          color: Colors.orange[400],
+                          onTap: () {},
+                        ),
+                        CustomIconButton(
+                          iconData: Icons.phone,
+                          color: Colors.green,
+                          onTap: () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
