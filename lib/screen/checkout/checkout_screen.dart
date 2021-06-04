df82de469ebf23a03d472553c782f66094796b7c
Pablo Guilherme Oliveira <pablogui1611@gmail.com>
2021-06-03 23:51:52 -0300

    forma pgto

--- lib/screen/checkout/checkout_screen.dart
+++ lib/screen/checkout/checkout_screen.dart
@@ -5,9 +5,11 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
+import 'components/credit_card_widget.dart';
+
 class CheckoutScreen extends StatelessWidget {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProxyProvider<CartManager, CheckoutManager>(
@@ -21,50 +23,67 @@ class CheckoutScreen extends StatelessWidget {
           title: const Text('Pagamento'),
           centerTitle: true,
         ),
-        body: Consumer<CheckoutManager>(
-          builder: (_, checkoutManager, __) {
-            if (checkoutManager.loading) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Consumer<CheckoutManager>(
+            builder: (_, checkoutManager, __) {
+              if (checkoutManager.loading) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        'Processando seu pagamento...',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16),
+                      )
+                    ],
+                  ),
+                );
+              }
+
+              return Form(
+                key: formKey,
+                child: ListView(
                   children: <Widget>[
-                    CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation(Colors.white),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      'Processando seu pagamento...',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16),
+                    CreditCardWidget(),
+                    PriceCard(
+                      buttonText: 'Finalizar Pedido',
+                      onPressed: () {
+                        if (formKey.currentState.validate()) {
+                          print('enviar');
+                          /*checkoutManager.checkout(
+                              onStockFail: (e){
+                                Navigator.of(context).popUntil(
+                                        (route) => route.settings.name == '/cart');
+                              },
+                              onSuccess: (order){
+                                Navigator.of(context).popUntil(
+                                        (route) => route.settings.name == '/');
+                                Navigator.of(context).pushNamed(
+                                    '/confirmation',
+                                    arguments: order
+                                );
+                              }
+                          );*/
+                        }
+                      },
                     )
                   ],
                 ),
               );
-            }
-
-            return ListView(
-              children: <Widget>[
-                PriceCard(
-                  buttonText: 'Finalizar Pedido',
-                  onPressed: () {
-                    checkoutManager.checkout(onStockFail: (e) {
-                      Navigator.of(context)
-                          .popUntil((route) => route.settings.name == '/cart');
-                    }, onSuccess: (order) {
-                      Navigator.of(context)
-                          .popUntil((route) => route.settings.name == '/');
-                      Navigator.of(context)
-                          .pushNamed('/confirmation', arguments: order);
-                    });
-                  },
-                )
-              ],
-            );
-          },
+            },
+          ),
         ),
       ),
     );
